f7290f8268d14bade197e39953682d05a776b6d2
unknown <arkangel.w@gmail.com>
2017-09-13 00:16:57 -0400

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraDeNavegacionComponent } from './barra-de-navegacion/barra-de-navegacion.component';
+import { BarraComponent } from './barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraDeNavegacionComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
